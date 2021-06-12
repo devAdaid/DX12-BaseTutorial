46d5dfaf937f43ca5181e24db69e7baffa671a33
devAdaid <ahyeong2533@gmail.com>
2021-06-12 14:37:37 +0900

    클래스 생성
    Ctr+w: 워드 선택
    Alt+Shift: 멀티 커서

--- DX12-BaseTutorial/main.c
+++ DX12-BaseTutorial/main.c
@@ -1,11 +1,29 @@
 #include <Windows.h>
 
-int wWinMain(
+int WINAPI wWinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR lpCmdLine,
 	_In_ int nShowCmd
 )
 {
+	WNDCLASS wndClass;    
+	wndClass.style = 0;
+	wndClass.lpfnWndProc = NULL;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInstance;
+	wndClass.hIcon = NULL;
+	wndClass.hCursor = NULL;
+	wndClass.hbrBackground = NULL;
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = L"DX12BaseTutorialClass";
+	ATOM atom = RegisterClass(&wndClass);
 
+	if (atom == 0)
+	{
+		return 1;
+	}
+
+	return 0;
 }
