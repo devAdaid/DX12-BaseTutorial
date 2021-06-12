b3e87943dd0eb3eb852a4e2d612359cff1ff05f5
devAdaid <ahyeong2533@gmail.com>
2021-06-12 15:13:47 +0900

    블럭 나눠주기
    Ctr + MM: 블럭 접기 전환

--- DX12-BaseTutorial/main.c
+++ DX12-BaseTutorial/main.c
@@ -22,21 +22,25 @@ int WINAPI wWinMain(
 	_In_ int nShowCmd
 )
 {
-	WNDCLASS wndClass;    
-	wndClass.style = 0;
-	wndClass.lpfnWndProc = WndProc;
-	wndClass.cbClsExtra = 0;
-	wndClass.cbWndExtra = 0;
-	wndClass.hInstance = hInstance;
-	wndClass.hIcon = NULL;
-	wndClass.hCursor = NULL;
-	wndClass.hbrBackground = NULL;
-	wndClass.lpszMenuName = NULL;
-	wndClass.lpszClassName = L"DX12BaseTutorialClass";
-	ATOM atom = RegisterClass(&wndClass);
-	RETURN_IF_ZERO(atom);
+	{
+		WNDCLASS wndClass;
+		wndClass.style = 0;
+		wndClass.lpfnWndProc = WndProc;
+		wndClass.cbClsExtra = 0;
+		wndClass.cbWndExtra = 0;
+		wndClass.hInstance = hInstance;
+		wndClass.hIcon = NULL;
+		wndClass.hCursor = NULL;
+		wndClass.hbrBackground = NULL;
+		wndClass.lpszMenuName = NULL;
+		wndClass.lpszClassName = L"DX12BaseTutorialClass";
+		ATOM atom = RegisterClass(&wndClass);
+		RETURN_IF_ZERO(atom);
+	}
+
 	HWND hWnd = CreateWindow(L"DX12BaseTutorialClass", L"DX12", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
 		100, 0, 300, 200, NULL, NULL, hInstance, NULL);
+
 	while (gQuit == FALSE)
 	{
 		MSG msg;
